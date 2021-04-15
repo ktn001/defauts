f05222fd962252621e95441fd5b3caf781b6ece2
Christian <chris@localhost.localdomain>
2021-04-15 22:28:02 +0200

    ...

--- desktop/js/defauts.js
+++ desktop/js/defauts.js
@@ -89,6 +89,7 @@ function addCmdToTable(_cmd) {
 	tr += '<td/>';
 	tr += '<td/>';
 	tr += '<td/>';
+	tr += '<td/>';
 	tr += '<td>';
 	tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
 	tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
@@ -107,6 +108,7 @@ function addCmdToTable(_cmd) {
 	tr += '</td>';
 	/* NOM */
 	tr += '<td><input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom surveillance}}"></td>';
+	tr += '<td/>';
 	tr += '<td/>';
 	tr += '<td/>';
 	tr += '<td/>';
@@ -148,22 +150,19 @@ function addCmdToTable(_cmd) {
 	tr += '</td>';
 	/* LIMITE */
 	tr += '<td>';
-	tr += '<div">';
-	tr += '<input class="cmdAttr form-control input-sm tooltips" style="width:48%; float:left" data-l1key="configuration" data-l2key="limite" title="{{Valeur devant être atteinte après enclechement}}" placeholder="{{Limite}}">';
-	tr += '<input class="cmdAttr form-control input-sm tooltips" style="width:48%; float:right" data-l1key="configuration" data-l2key="delais" title="{{Attente après changement d\'état}}" placeholder="{{secondes}}">';
-	tr += '</div>';
-	tr += '<div>';
-	tr += '<label class="tooltips" style="width:44%" title="{{Inversion du test de la limite}}"><input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="invert"/>{{Inverser}}</label>';
-	tr += '<label class="tooltips" style="width:28%" title="{{Surveillance si etat =1}}"><input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="en"/>{{En}}</label>';
-	tr += '<label class="tooltips" style="width:28%" title="{{Surveillance si etat =0}}"><input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="hors"/>{{hors}}</label>';
-	tr += '</div>';
+	tr += '<input class="cmdAttr form-control input-sm tooltips" data-l1key="configuration" data-l2key="limite" title="{{Valeur devant être atteinte après enclechement}}" placeholder="{{Limite}}">';
+	tr += '</td>';
+	/* TEMPORISATION */
+	tr += '<td>';
+	tr += '<input class="cmdAttr form-control input-sm tooltips" data-l1key="configuration" data-l2key="delais" title="{{Attente après changement d\'état}}" placeholder="{{secondes}}">';
 	tr += '</td>';
 	/* OPTIONS */
 	tr += '<td>';
-	tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
-	tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span> ';
-	tr += '</td>';
-
+	tr += '<label class="checkbox-inline tooltips" title="{{Inversion du test de la limite}}"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="configuration" data-l2key="invert"/>{{Inverser}}</label>';
+	tr += '<label class="checkbox-inline tooltips" title="{{Surveillance pour etat =1}}"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="configuration" data-l2key="en" checked/>{{En}}</label>';
+	tr += '<label class="checkbox-inline tooltips" title="{{Surveillance pour etat =0}}"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="configuration" data-l2key="hors" checked/>{{hors}}</label>';
+	tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label>';
+	tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label>';
 	tr += '<td>';
 	if (is_numeric(_cmd.id)) {
 	    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
