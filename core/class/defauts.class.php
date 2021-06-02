87b01afeb13e65dc8d05cf14e20820d6137a100a
ktn001 <christian.kolly@hispeed.ch>
2021-06-02 18:52:20 +0200

    alignament de l'historique

--- core/class/defauts.class.php
+++ core/class/defauts.class.php
@@ -438,7 +438,7 @@ class defautsCmd extends cmd {
 	public function actualiseHistorique($liste) {
 		if ($this->getLogicalId() == 'historique') {
 			$this->setCache("liste", json_encode($liste));
-			$value = "";
+			$value .= "<div style='text-align:left'>";
 			$nbLignes = $this->getConfiguration("histosize");
 			for ($i = 0; $i < $nbLignes; $i++) {
 				$value .= "<br/>";
@@ -446,7 +446,7 @@ class defautsCmd extends cmd {
 					$value .= date("d-m H:i:s",$liste[$i]["time"]) . " : " . $liste[$i]["nom"];
 				}
 			}
-			$value .= "<br/><br/>";
+			$value .= "<br/><br/></div>";
 			return $value;
 		} else {
 			log::add("defauts","erreur","actualiseHistorique  appelé pour une commande erronée");
