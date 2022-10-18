3b0948843dce5a096d81f69595f466c94e53595b
ktn <jeedom@7ea6d355132c>
2022-10-18 18:22:47 +0200

    adaptation tableau cmd pour jeedom 4.3

--- desktop/php/defauts.php
+++ desktop/php/defauts.php
@@ -166,11 +166,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				    <div>{{Etat}}</div>
 				    <div>{{Mesure}}</div>
 				</th>
-				<th style="width:400px">
+				<th style="width:200px">
 				    <div>{{Consigne}}</div>
 				    <div><span style="width:80px; display:inline-block">{{Limite}}</span><span>{{Temporisation}}</span></div>
 				</th>
-				<th style="width:340px">{{paramètres}}</th>
+				<th style="width:380px">{{paramètres}}</th>
 				<th style="width:200px">{{Options}}</th>
 				<?php
 				if (version_compare(jeedom::version(), '4.3.0', '>=')) {
